1e709edffd557a13f26cc4b69ea14a7618159f8f
gabriel-vitebo <vitebo@outlook.com>
2024-07-03 18:33:15 -0300

    add get and set

--- src/domain/entities/game.ts
+++ src/domain/entities/game.ts
@@ -1,11 +1,62 @@
 import { Slug } from "./value-object/slug"
 import { Entity } from "../../core/entities/entity"
+import { Optional } from "../../core/types/optional"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface GameProps {
+  userId: string,
   title: string
   numberOfAchievements: number
   slug: Slug
+  createdAt: Date
+  updateAt?: Date
 }
 
 export class Game extends Entity<GameProps> {
+  get userId() {
+    return this.props.userId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get numberOfAchievements() {
+    return this.props.numberOfAchievements
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updateAt() {
+    return this.props.updateAt
+  }
+
+  private touch() {
+    this.props.updateAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  static create(
+    props: Optional<GameProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityId
+  ) {
+    const game = new Game({
+      ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
+      createdAt: new Date()
+    }, id)
+
+    return game
+  }
 }
--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,8 +1,42 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface UserProps {
   name: string
+  createdAt: Date
+  updateAt?: Date
 }
 
 export class User extends Entity<UserProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updateAt() {
+    return this.props.updateAt
+  }
+
+  private touch() {
+    this.props.updateAt = new Date()
+  }
+
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
+  static create(
+    props: Optional<UserProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const user = new User({
+      ...props,
+      createdAt: new Date()
+    }, id)
+  }
 }
